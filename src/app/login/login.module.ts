286b37a2eed70143e919f9106b4fc58133c45834
Tristan Brandt <me@tristanbrandt.io>
2018-08-28 05:10:00 +0200

    remove login.module

--- src/app/login/login.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ButtonComponent } from '../button/button.component';
-import { LoaderComponent } from '../loader/loader.component';
-import { ErrorListComponent } from '../error-list/error-list.component';
-
-@NgModule({
-  declarations: [ButtonComponent, LoaderComponent, ErrorListComponent]
-})
-export class LoginModule {}
